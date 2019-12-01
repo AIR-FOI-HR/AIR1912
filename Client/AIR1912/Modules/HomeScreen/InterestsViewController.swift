9a1851c8f8e5d89d6c981bc39262650ba48e4cd9
Dalibor Šuplika <37180325+Dsuplika@users.noreply.github.com>
2019-12-01 17:26:54 +0100

    Izradi homescreen #25
    Izrađen je homescreen sa 2 carusel sekcije u kojoj su prikazane placeholder slike.

---
+++ Client/AIR1912/Modules/HomeScreen/InterestsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  InterestsViewController.swift
+//  AIR1912
+//
+//  Created by Infinum on 01/12/2019.
+//  Copyright © 2019 Leo Leljak. All rights reserved.
+//
+
+import UIKit
+
+class InterestsViewController : UIViewController
+{
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionView2: UICollectionView!
+    var interests = Interest.fetchInterests()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        collectionView.dataSource = self
+        collectionView2.dataSource = self
+    }
+}
+
+// UICollectionViewDataSource
+
+extension InterestsViewController:
+    UICollectionViewDataSource
+{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return interests.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell =
+        collectionView.dequeueReusableCell(withReuseIdentifier: "InterestCollectionViewCell", for: indexPath) as! InterestCollectionViewCell
+        let interest = interests[indexPath.item]
+        
+        cell.interest = interest
+        
+        return cell
+    }
+}
+
