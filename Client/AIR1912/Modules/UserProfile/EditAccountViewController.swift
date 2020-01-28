af8ebfc2fed45dc052f43ad92784139806ddf96c
Dalibor Šuplika <37180325+Dsuplika@users.noreply.github.com>
2020-01-28 13:59:53 +0100

    Dodaj prikaz korisnckih podataka #63
    Dodan je prikaz korisničkih podataka u klikom na postavke, te zatim account.

---
+++ Client/AIR1912/Modules/UserProfile/EditAccountViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EditAccountViewController.swift
+//  AIR1912
+//
+//  Created by Infinum on 28/01/2020.
+//  Copyright © 2020 Leo Leljak. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import CodableAlamofire
+
+class EditAccountViewController: UIViewController {
+    
+}
