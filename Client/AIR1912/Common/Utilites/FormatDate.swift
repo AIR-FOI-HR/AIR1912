bdbc209744b7934dceb4b94a8da841c41f918ea5
Leo Leljak <lleljak@foi.hr>
2020-01-28 22:19:58 +0100

    #50 Modularni prikaz
    - napravljena logika popunjavanja primljenog Eventa pri pozivu ViewControllera iz nekog drugog screena
    - napravljena modularnost za prikaz private/public eventa
    - modularnost za prikaz evenata koji su u vlasništvu modularno

--- Client/AIR1912/Common/Utilites/FormatDate.swift
+++ Client/AIR1912/Common/Utilites/FormatDate.swift
@@ -20,6 +20,17 @@ class FormatDate {
            return nil
        }
     
+    public static func getDateFromWebDBString(date: String?) -> NSDate? {
+        if let date1 = date {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd.MM.YYYY HH:mm"
+            dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
+            let date: NSDate? = dateFormatter.date(from: date1) as NSDate?
+            return date!
+        }
+        return nil
+    }
+    
     public static func getStringFromDate(date: NSDate?) -> String? {
         if let date1 = date {
             let dateFormatter = DateFormatter()
@@ -30,4 +41,23 @@ class FormatDate {
         }
         return nil
     }
+    
+    public static func getNameOfDayFromDate(date: NSDate?) -> String?{
+           let dateFormatter = DateFormatter()
+        var weekday: String = ""
+           dateFormatter.dateFormat = "cccc"
+        weekday = dateFormatter.string(from: date! as Date)
+        return weekday
+    }
+    
+    public static func getNameOfMonthFromDate(date: NSDate?) -> String?{
+              let dateFormatter = DateFormatter()
+           var weekday: String = ""
+              dateFormatter.dateFormat = "MMMM"
+           weekday = dateFormatter.string(from: date! as Date)
+           return weekday
+       }
+    
+   
+    
 }
--- Client/AIR1912/Modules/EventDetails/EventDetailsViewController.swift
+++ Client/AIR1912/Modules/EventDetails/EventDetailsViewController.swift
@@ -2,56 +2,191 @@
 //  EventDetailsViewController.swift
 //  AIR1912
 //
-//  Created by Leo Leljak on 09/12/2019.
-//  Copyright © 2019 Leo Leljak. All rights reserved.
+//  Created by Leo Leljak on 28/01/2020.
+//  Copyright © 2020 Leo Leljak. All rights reserved.
 //
 
 import UIKit
-import Spring
-
+import CoreLocation
+import KBRoundedButton
+import Floaty
 class EventDetailsViewController: UIViewController {
-
     
-    //MARK: -IBOutlets
-    @IBOutlet weak var cardView: SpringView!
-    @IBOutlet weak var belowSubview: SpringView!
-    @IBOutlet weak var backgroundImage: UIImageView!
+    //iboutlets
+    @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var frontImage: UIImageView!
+    @IBOutlet weak var subView: UIView!
+    @IBOutlet weak var backImage: UIImageView!
+    @IBOutlet weak var titleEvent: UILabel!
+    @IBOutlet weak var eventType: UILabel!
+    @IBOutlet weak var lockEvent: UIImageView!
+    @IBOutlet weak var labelDay: UILabel!
+    @IBOutlet weak var labelTime: UILabel!
+    @IBOutlet weak var labelNameOfDay: UILabel!
+    @IBOutlet weak var labelLocation1: UILabel!
+    @IBOutlet weak var textLocation2: UITextView!
+    @IBOutlet weak var labelNumver: UILabel!
+    @IBOutlet weak var textDescription: UITextView!
     
-    //MARK: -Properties
-    let imageBlur = ImageBlur()
+    @IBOutlet weak var buttonJoin: KBRoundedButton!
+    @IBOutlet weak var floatyButton: Floaty!
+    
+    
+    
+    //properties
+    var event:Event = Event()
+    let cornerRadius : CGFloat = 12
+    let keychain = UserKeychain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //view.backgroundColor = .clear
-        setBlurredImage()
-        belowSubview.isHidden = false
+       
+        setShadowView()
+        configure()
         
-        cardView.isHidden = false
-       // cardView.animate()
+       
+        floatyButton.verticalDirection = .down
+        floatyButton.buttonColor =  UIColor(hex:  "97FFA8")
+    
+        let update = FloatyItem()
+        update.icon = UIImage(named: "update")
+        update.buttonColor = UIColor(hex:  "97FFA8")
+        update.imageSize = CGSize(width: 20, height: 20)
+        floatyButton.addItem(item: update)
+        let trash = FloatyItem()
+        trash.icon = UIImage(named: "trash")
+        trash.buttonColor = UIColor(hex:  "FF1306")
+        floatyButton.addItem(item: trash)
+        
         
         
     }
     
-    
-    func setBlurredImage() {
-        
-        
-        
-        backgroundImage.image = UIImage(named: "testMovie")!
-        let blur = UIVisualEffectView(effect: UIBlurEffect(style: .systemChromeMaterialDark))
-        blur.frame = self.view.bounds
-        self.view.insertSubview(blur, aboveSubview: backgroundImage)
-        
-    }
 
     
-    @IBAction func swipeExit(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+
+}
+
+//Initial set
+extension EventDetailsViewController{
+    func setShadowView() {
+        
+        shadowView.layer.cornerRadius = cornerRadius
+        shadowView.layer.shadowColor = UIColor.darkGray.cgColor
+        shadowView.layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+        shadowView.layer.shadowRadius = 22.0
+        shadowView.layer.shadowOpacity = 0.6
+      
+        frontImage.layer.cornerRadius = cornerRadius
+        frontImage.clipsToBounds = true
+        subView.layer.cornerRadius = 12
+     
+    }
+    
+    func setBlurredImage(poster : URL?) {
+        
+        backImage.kf.setImage(with: poster)
+        let darkBlur = UIBlurEffect(style: .systemUltraThinMaterialDark)
+        let blurView = UIVisualEffectView(effect: darkBlur)
+        blurView.frame = backImage.bounds
+        backImage.addSubview(blurView)
         
     }
     
-    @IBAction func tapExit(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    func setUpView(for content: DBContent) {
+        self.frontImage.kf.setImage(with: URL(string:  content.posterURL!))
+        self.setBlurredImage(poster: URL(string:  content.posterURL!))
+        
     }
     
+    func configure(){
+        
+        if(event.isPrivate==1){
+            confIfPrivate()
+        }
+        if(event.ownerId == keychain.getID() ){
+            self.floatyButton.isHidden = false
+        }
+        
+        let ContentProvider = WebContentProvider()
+        ContentProvider.getContentById(for: event.contentID) { (result) in
+            switch (result){
+            case .success(let content):
+                self.setUpView(for: content[0])
+                self.eventType.text = content[0].type
+            case .failure( _):
+                    print("Nije nađen content u bazi")
+                    
+            }
+        }
+        
+        self.titleEvent.text = event.title
+        
+        if(event.isPrivate==1){
+            lockEvent.isHidden = false
+        }
+        print (event.dateTime as Any)
+        let dateFromString = FormatDate.getDateFromWebDBString(date: event.dateTime)
+        let day = FormatDate.getNameOfDayFromDate(date: dateFromString)
+        let nameOfMonth = FormatDate.getNameOfMonthFromDate(date: dateFromString)
+        
+        let calendar = Calendar.current
+        let comp = calendar.dateComponents([.day, .hour, .minute, .year], from: dateFromString as! Date)
+        
+        let dayOfMonth = comp.day
+        labelDay.text = "\(dayOfMonth!).\(nameOfMonth!)"
+        labelNameOfDay.text = "\(day!)"
+        
+        
+        let hour = comp.hour
+        let minute = comp.minute
+        labelTime.text = "\(hour!):\(minute!)"
+       
+        geocode(latitude: event.latitude, longitude: event.longitude) { placemark, error in
+            guard let placemark = placemark, error == nil else { return }
+            
+            DispatchQueue.main.async {
+                
+                self.textLocation2.text = placemark.thoroughfare ?? ""
+            }
+        }
+        
+        self.textDescription.text = event.description
+        
+        let maxNumber = event.maxNumberOfPeople
+        var currentNumber = 0
+        if(event.numberOfPeople != nil){
+            currentNumber = event.numberOfPeople!
+        }
+        self.labelNumver.text = "\(currentNumber)/\(maxNumber)"
+        
+        
+
+        
+        
+       
+        
+        
+        
+        
+        
+        
+        
+     
+        
+    }
+    
+}
+
+extension EventDetailsViewController{
+    
+    func confIfPrivate(){
+        buttonJoin.backgroundColorForStateNormal = UIColor(hex: "FF1306")
+        shadowView.backgroundColor = UIColor(hex: "FF1306").withAlphaComponent(0.55)
+        
+    }
+  
+    func geocode(latitude: Double , longitude: Double, completion: @escaping (CLPlacemark?, Error?) -> ())  {
+               CLGeocoder().reverseGeocodeLocation(CLLocation(latitude: latitude, longitude: longitude)) { completion($0?.first, $1) }
+           }
 }
