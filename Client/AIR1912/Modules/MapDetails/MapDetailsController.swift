da8bdc56c61e09f32b170aaea482c2d4f7f413dd
dmartan <dmartan@foi.hr>
2020-01-29 13:22:17 +0100

    #61 Prikaz detalja
    Kratki pregled detalja dogadaja uz mogucnost klika na gumb Show more koji otvara prozor sa cijelim eventom.

--- Client/AIR1912/Modules/MapDetails/MapDetailsController.swift
+++ Client/AIR1912/Modules/MapDetails/MapDetailsController.swift
@@ -11,14 +11,30 @@ import UIKit
 class MapDetailsController: UIViewController {
  
     @IBOutlet weak var eventTitleTxt: UITextField!
+    @IBOutlet weak var eventDateTimeTxt: UITextField!
+    @IBOutlet weak var eventDescriptionTxt: UITextField!
+    
+    @IBOutlet weak var eventNumberOfPeopleTxt: UITextField!
+    
+    @IBAction func showMoreButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "EventDetails", bundle: nil)
+          guard let viewController = storyboard.instantiateViewController(identifier: "EventDetails") as? EventDetailsViewController else {
+              return
+          }
+        
+          viewController.modalPresentationStyle = .formSheet
+          
+          // na view controller detaljnog prikaza pridodaj odabrani event
+        viewController.event = selectedEvent!
+          
+          self.present(viewController, animated: true, completion: nil)
+    }
     
     var selectedEvent: Event?
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print(selectedEvent)
         
         displayEventDetails()
         // Do any additional setup after loading the view.
@@ -27,6 +43,17 @@ class MapDetailsController: UIViewController {
 
     private func displayEventDetails(){
         eventTitleTxt.text = selectedEvent?.title
+        eventDateTimeTxt.text = selectedEvent?.dateTime
+        eventDescriptionTxt.text = selectedEvent?.description
+        if(selectedEvent?.numberOfPeople == nil){
+            eventNumberOfPeopleTxt.text = "0"
+        }
+        else{
+            eventNumberOfPeopleTxt.text = " \(String(describing: selectedEvent?.numberOfPeople))"
+        }
+        
+        
+        
         
     }
 }
